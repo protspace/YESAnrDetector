468bec3f871fa32d43a92793f2931a4c28aa2de5
Yevhen Sahidulin <sagiduline@mydigicode.com>
2019-04-22 16:11:18 +0300

    Initial Commit

---
+++ ANRDetector_Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ANRDetector_Demo
+//
+//  Created by Yevhen Sahidulin on 4/22/19.
+//  Copyright © 2019 Yevhen Sagidulin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ANRDetector_DemoTests/ANRDetector_DemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  ANRDetector_DemoTests.swift
+//  ANRDetector_DemoTests
+//
+//  Created by Yevhen Sahidulin on 4/22/19.
+//  Copyright © 2019 Yevhen Sagidulin. All rights reserved.
+//
+
+import XCTest
+@testable import ANRDetector_Demo
+
+class ANRDetector_DemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
